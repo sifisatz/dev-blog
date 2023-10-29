a9294ad85105222401d6dd8e0d279887cba6afb0
조셉 <iwsifa@gmail.com>
2023-10-30 00:33:40 +0200

    rename

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home  () {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 
       {posts.map((post) => (
-         <Link key={post.id} href={'/post/[id]'} as={`/post/${post.id}`}>
+         <Link key={post.id} href={'/blog/[id]'} as={`/blog/${post.id}`}>
           {post.title}
       </Link>))}
 
